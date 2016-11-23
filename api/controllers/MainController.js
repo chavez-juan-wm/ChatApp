3cea718e890c80d8d6c9cd14a2d5d3c2025eea89
Juan Chavez <juan.chavez@west-mec.org>
2016-11-22 18:12:54 -0700

    Added some stuff

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -34,9 +34,32 @@ module.exports = {
         });
     },
     login: function (req, res) {
+        var username = req.param("username");
+        var password = req.param("password");
 
+        Users.findByUsername(username).done(function(err, usr) {
+            if (err) {
+                res.send(500, { error: "DB Error" });
+            } else {
+                if (usr) {
+                    var hasher = require("password-hash");
+                    if (hasher.verify(password, usr.password)) {
+                        req.session.user = usr;
+                        res.send(usr);
+                    } else {
+                        res.send(400, { error: "Wrong Password" });
+                    }
+                } else {
+                    res.send(404, { error: "User not Found" });
+                }
+            }
+        });
     },
     chat: function (req, res) {
-
+        if (req.session.user) {
+            res.view({username: req.session.user.username});
+        } else {
+            res.redirect('/');
+        }
     }
 };
